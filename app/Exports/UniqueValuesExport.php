3b393b3ba361dd73b7847d27fe1cf49b2160333e
Cheran Sengodan <cheran.sengodan@sensiple.com>
2023-10-10 19:58:47 +0530

    export completed

---
+++ app/Exports/UniqueValuesExport.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+class UniqueValuesExport implements FromCollection
+{
+    public $data;
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function collection()
+    {
+        return collect($this->data);
+    }
+}
--- app/Http/Controllers/DuplicateRemoveController.php
+++ app/Http/Controllers/DuplicateRemoveController.php
@@ -14,39 +14,36 @@ use PHPUnit\Event\TestSuite\Loaded;
 
 class DuplicateRemoveController extends Controller
 {
-     
-    // view duplicate remove page
-       public function viewDuplicatePage()
-       {
-         
-   
-          return view('admin.forms.lead.exportlead');
 
- 
-       }
+  // view duplicate remove page
+  public function viewDuplicatePage()
+  {
 
-    // logic for remove 
 
-    public function checkDuplicateValue(Request $request){
-      
-        // request excel file
-        $file = $request->file('check-data');
-       
-        $data = Excel::toArray([], $file);
-      
-        $collect_data = collect($data[0])->flatten(1);
-        // collect unique data
-        $unique_data = array_unique($collect_data->toArray());
-        
-        // get leads mobile numbes
-         $mobile_number = Lead::pluck('mobile_number')->toArray(); 
-    
-        //  remove duplicate values
-          $remove_dupli_values =array_values(array_diff($unique_data,$mobile_number));
-        return $remove_dupli_values;
-               
-        // return Excel::download(new UniqueValuesExport($remove_dupli_values),$filename,\Maatwebsite\Excel\Excel::XLSX);       
-       
-    }
+    return view('admin.forms.lead.exportlead');
+  }
 
+  // logic for remove 
+
+  public function checkDuplicateValue(Request $request)
+  {
+
+    // request excel file
+    $file = $request->file('check-data');
+
+    $data = Excel::toArray([], $file);
+
+    $collect_data = collect($data[0])->flatten(1);
+    // collect unique data
+    $unique_data = array_unique($collect_data->toArray());
+
+    // get leads mobile numbes
+    $mobile_number = Lead::pluck('mobile_number')->toArray();
+
+    //  remove duplicate values
+    $remove_dupli_values = array_chunk(array_values(array_filter(array_diff($unique_data, $mobile_number))), 1);
+    // return $remove_dupli_values;
+
+    return Excel::download(new UniqueValuesExport($remove_dupli_values), 'unique_numbers.xlsx');
+  }
 }
